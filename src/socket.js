8d56e49e4eee7a1ae06ffce9e126232ebaffe72e
Alikhan67595 <hanbal22hassan@gmail.com>
2025-12-25 04:23:30 +0500

    Socket.io

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import {io} from "socket.io-client";
 
-const socket = io('https://whats-app-backend-roan.vercel.app',{withCredentials:true})   
+const socket = io('https://welcome-charmine-alikhan67595-a5ec3999.koyeb.app',{withCredentials:true})   
 
 export default socket;
